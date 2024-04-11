405092f706008bc18d8736f43c9ccea54d70889b
Leonardo Tumadjian <tumadjian@gmail.com>
2024-04-11 13:46:57 -0300

    Add more parameters to cors and comments

--- config/routes.php
+++ config/routes.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Application\Controllers\HealthCheckController;
 use Application\Framework\Decorators\GlobalCors;
-
 use Slim\App;
-use Slim\Routing\RouteCollectorProxy;
+use Slim\Exception\HttpNotFoundException;
 
 /**
  * IIFE to isolate execution and global variables
@@ -18,8 +17,16 @@ return (function (App $app) {
 
     $app->get('/health-check', HealthCheckController::class);
 
-    $app->group('/v1', function (RouteCollectorProxy $group) {
+    // Your routes definitions here
+    // You can config route in your preferred directory
+    // $app = require __DIR__ . '/../app/Products/routes.php';
 
+    /**
+     * Catch-all route to serve a 404 Not Found page if none of the routes match
+     * NOTE: make sure this route is defined last
+     */
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($request, $response) {
+        throw new HttpNotFoundException($request);
     });
 
     return $app;
